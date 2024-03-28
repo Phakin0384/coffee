c3b2a4a5e5bd8f5b1ecaeea436b8d2b1f2e81163
Phakin0384 <phakinza1824@gmail.com>
2024-03-28 22:57:27 +0700

    Yeet

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Menu from './frontend/menu';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Heehee</Text>
-      <StatusBar style="auto" />
+    <Menu/>
     </View>
   );
 }
