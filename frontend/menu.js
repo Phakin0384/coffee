c335f48bb54005dd8740412eba8e1a01c05cc5f2
Poopamaster <poopazizi16@gmail.com>
2024-03-28 23:17:16 +0700

    font

--- frontend/menu.js
+++ frontend/menu.js
@@ -5,25 +5,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>กดเพื่อเลือกเมนู</Text>
-      <Text>Mocca</Text>
-      <Text>Americano</Text>
-      <Text>Espresspo</Text>
-      <Text>CAPUCHINO</Text>
-      <Text>Latte</Text>
+      <Text style={styles.menuItem}>กดเพื่อเลือกเมนู</Text>
+      <Text style={styles.menuItem}>Mocca</Text>
+      <Text style={styles.menuItem}>Americano</Text>
+      <Text style={styles.menuItem}>Espresso</Text>
+      <Text style={styles.menuItem}>CAPPUCCINO</Text>
+      <Text style={styles.menuItem}>Latte</Text>
       <StatusBar style="auto" />
     </View>
-
-    
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EEE8DC',
+    backgroundColor: '#F5F5DC', // Background color fills the entire screen
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  menuItem: {
+    marginBottom: 10, // Add space between each menu item
+    fontFamily: 'Arial', // Change the font
+    fontSize: 20, // Set the font size
   },
 });
