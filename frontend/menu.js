a9ab17f67ec4abff9b02e5a5610feda31f6011fb
Poopamaster <poopazizi16@gmail.com>
2024-03-28 23:12:32 +0700

    name menu

--- frontend/menu.js
+++ frontend/menu.js
@@ -5,16 +5,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ya Yeet!</Text>
+      <Text>กดเพื่อเลือกเมนู</Text>
+      <Text>Mocca</Text>
+      <Text>Americano</Text>
+      <Text>Espresspo</Text>
+      <Text>CAPUCHINO</Text>
+      <Text>Latte</Text>
       <StatusBar style="auto" />
     </View>
+
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EEE8DC',
     alignItems: 'center',
     justifyContent: 'center',
   },
