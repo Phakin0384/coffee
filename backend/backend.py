762938f4e51b94d01d441677a35267b736dd7c06
Phakin0384 <phakinza1824@gmail.com>
2024-03-30 02:10:52 +0700

    adjust backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,20 +1,62 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify,request
+from flask_cors import CORS, cross_origin
+from pymongo.mongo_client import MongoClient
+import pymongo
+
+uri = "mongodb+srv://Phakin1824:Geng_100104@cluster0.pzjda8i.mongodb.net/"
+client = MongoClient(uri)
+db = client["CoffeeShop"]
+collection = db["coffee"]
+p_in_DB = collection.find()
+coffee=[]
+for p in p_in_DB:
+        coffee.append(p)
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Define a list of menu items
-menu_items = [
-    {"id": 1, "name": "Mocca", "price": 3.5},
-    {"id": 2, "name": "Americano", "price": 2.5},
-    {"id": 3, "name": "Espresso", "price": 2.0},
-    {"id": 4, "name": "Cappuccino", "price": 3.0},
-    {"id": 5, "name": "Latte", "price": 3.0}
-]
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-# Endpoint to get menu items
-@app.route('/api/menu', methods=['GET'])
-def get_menu():
-    return jsonify(menu_items)
+@app.route("/coffee",methods=["GET"])
+def get_all_coffee():
+    return jsonify(coffee),200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+@app.route("/bill", methods=["POST"])    #register_account
+@cross_origin()
+def bill():
+    data = request.get_json()
+    
+    
+    count = len(coffee)
+    cof = 0
+    if count != 0:
+        cof = coffee[-1]["_id"] + 1
+    
+    new_coffee = {
+        "_id": cof,
+        "name": data["username"],
+        "sweetness": data["sweetness"],
+        "price": data["price"],
+        "temp": data["temp"]
+    }
+    
+    coffee.append(new_coffee)
+    
+    try:
+        # Insert into MongoDB
+        collection.insert_one(new_coffee)
+    except pymongo.errors.PyMongoError as e:
+        return jsonify({"error": str(e)}), 500  # Internal Server Error
+    
+    return jsonify(coffee), 200
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
