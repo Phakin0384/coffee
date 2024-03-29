6269ae9d8efa42896a277e85a23d274eca0a2457
Phakin0384 <phakinza1824@gmail.com>
2024-03-30 03:48:03 +0700

    cancle test

--- frontend/menu.js
+++ frontend/menu.js
@@ -6,18 +6,7 @@ import axios from 'axios';
 export default function App() {
   
   const navigation = useNavigation();
-  const axios = require('axios')
-  const test1Press = async () => {
-    try
-    {
-      await axios.post('https://mynodeserver.com/api/debug/throw', {timeout: 2000})
-      console.log("post call passed")
-    }
-    catch (err)
-    {
-      console.log("post call failed")
-    }
-  }
+  
   const handleMenuPress = (menuItem) => {
     switch (menuItem) {
       case "Mocca":
