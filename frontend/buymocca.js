061be3ad0bce2d4dbf74dedf2b71163c0d3b072e
Phakin0384 <phakinza1824@gmail.com>
2024-03-29 23:01:31 +0700

    holding selected state

--- frontend/buymocca.js
+++ frontend/buymocca.js
@@ -66,9 +66,6 @@ export default function App() {
         <Text style={[styles.text, temperature === 'cold' && styles.selected]}>เย็น</Text>
       </TouchableOpacity>
 
-      <View style={styles.textselectcancel}>
-        <Text style={styles.text}>ยกเลิก</Text>
-      </View>
 
       <View style={styles.textselectcoreect}>
         <Text style={styles.text}>ยืนยันคำสั่งซื้อ</Text>
@@ -99,13 +96,13 @@ const styles = StyleSheet.create({
   Hotimage: {
     width: 30,
     height: 40,
-    top: 95,
+    top: 75,
     right: 130
   },
   Coldimage: {
     width: 40,
     height: 40,
-    top: 15,
+    top: -5,
     left: 130,
   },
   textBox: {
@@ -123,6 +120,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Arial',
   },
   textSweet: {
+    
     paddingHorizontal: 30,
     paddingVertical: 10,
     borderRadius: 20,
@@ -132,263 +130,77 @@ const styles = StyleSheet.create({
     top: 140,
   },
   textselectsweet25: {
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
     paddingHorizontal: 10,
     paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
     top: 155,
     right: 130,
   },
   textselectsweet50: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
+    paddingHorizontal: 10, // กำหนดระยะห่างภายในกรอบด้านข้าง
+    paddingVertical: 5, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
     top: 120,
     right: 40,
   },
   textselectsweet75: {
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
     paddingHorizontal: 10,
     paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
     top: 83,
     left: 50,
   },
   textselectsweet100: {
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
     paddingHorizontal: 10,
     paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
     top: 46,
     left: 140,
   },
   textselecthot: {
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
     paddingHorizontal: 20,
     paddingVertical: 5,
-    top: 60,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
+    top: 40,
     right: 60,
   },
   textselectcold: {
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
     paddingHorizontal: 20,
     paddingVertical: 5,
-    top: -21,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
+    top: -41,
     left: 60,
   },
-  textselectcancel: {
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    top: 20,
-    right: 90,
-  },
   textselectcoreect: {
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    top: -22,
-    left: 80,
+    borderWidth: 2, // กำหนดความหนาของกรอบ
+    borderColor: 'black', // กำหนดสีของกรอบ
+    paddingHorizontal: 30,
+    paddingVertical: 8,
+    borderRadius: 20,
+    backgroundColor: '#E8FFC1',
+    top: -10,
+    left: 0,
   },
   selected: {
-    backgroundColor: '#F0C419', // Change to your desired color when selected
+    backgroundColor: '#F1A4A4', 
+    borderRadius: 20,
   }
 });
-
-// import React from 'react';
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
-
-// export default function App() {
-//   const Mocca = {
-//     uri: 'https://www.everyday-delicious.com/wp-content/uploads/2021/05/caffee-mocha-kawa-mokka-everyday-delicious-1-1197x1800.jpg',
-//   };
-//   const Hot = {
-//     uri: 'https://www.freeiconspng.com/uploads/black-flame-icon-png-24.png',
-//   };
-//   const Cold = {
-//     uri: 'https://static.thenounproject.com/png/1184671-200.png',
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Image source={Mocca} style={styles.Moccaimage}/>
-//       <View style={styles.textBox}>
-//         <Text style={styles.text}>Mocca</Text>
-//         </View>
-
-//         <View style={styles.textSweet}>
-//         <Text style={styles.text}>ระดับความหวาน</Text>
-//         </View>
-
-//         <View style={styles.textselectsweet25}>
-//         <Text style={styles.text}>25 %</Text>
-//         </View>
-
-//         <View style={styles.textselectsweet50}>
-//         <Text style={styles.text}>50 %</Text>
-//         </View>
-//         <View style={styles.textselectsweet75}>
-//         <Text style={styles.text}>75 %</Text>
-//         </View>
-//         <View style={styles.textselectsweet100}>
-//         <Text style={styles.text}>100 %</Text>
-//         </View>
-//         <Image source={Hot} style={styles.Hotimage}/>
-//         <View style={styles.textselecthot}>
-//         <Text style={styles.text}>ร้อน</Text>
-//         </View>
-//         <Image source={Cold} style={styles.Coldimage}/>
-//         <View style={styles.textselectcold}>
-//         <Text style={styles.text}>เย็น</Text>
-//         </View>
-//         <View style={styles.textselectcancel}>
-//         <Text style={styles.text}>ยกเลิก</Text>
-//         </View>
-//         <View style={styles.textselectcoreect}>
-//         <Text style={styles.text}>ยืนยันคำสั่งซื้อ</Text>
-//         </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#EEE8DC', // กำหนดสีพื้นหลังของทั้งหน้าจอ
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     position: 'relative',
-//     width:400,
-//   },
-//   Moccaimage: {
-//     width: 290, // กำหนดความกว้างของรูปภาพ
-//     height: 370, // กำหนดความสูงของรูปภาพ
-//     marginBottom: -100, // กำหนดระยะห่างจากข้อความด้านล่าง
-//     margin: 5,
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//     paddingVertical: 5,
-//     top: 25
-//   },
-//   Hotimage: {
-//     width: 30, // กำหนดความกว้างของรูปภาพ
-//     height: 40, // กำหนดความสูงของรูปภาพ
-//     top: 95,
-//     right: 130
-//   },
-//   Coldimage: {
-//     width: 40, // กำหนดความกว้างของรูปภาพ
-//     height: 40, // กำหนดความสูงของรูปภาพ
-//     top: 15,
-//     left: 130,
-//   },
-//   textBox: {
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     paddingHorizontal: 90, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderRadius: 20,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 130,
-//   },
-//   text: {
-//     fontSize: 20, // กำหนดขนาดของตัวอักษร
-//     fontFamily: 'Arial',
-//   },
-//   textSweet: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderRadius: 20,
-//     paddingHorizontal: 30,
-//     backgroundColor: '#EBCCCC',
-//     paddingVertical: 5,
-//     top: 140,
-//   },
-//   textselectsweet25: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 155,
-//     right: 130,
-//   },
-//   textselectsweet50: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 120,
-//     right: 40,
-//   },
-//   textselectsweet75: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 83,
-//     left: 50,
-//   },
-//   textselectsweet100: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 10,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 46,
-//     left: 140,
-//   },
-//   textselecthot: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 2, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 20,
-//     backgroundColor: '#FFFFFF',
-//     paddingVertical: 5,
-//     top: 60,
-//     right: 60,
-//   },
-//     textselectcold: {
-//       paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//       paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//       borderWidth: 2, // กำหนดความหนาของกรอบ
-//       borderColor: 'black', // กำหนดสีของกรอบ
-//       borderRadius: 20,
-//       paddingHorizontal: 20,
-//       backgroundColor: '#FFFFFF',
-//       paddingVertical: 5,
-//       top: -21,
-//       left: 60,
-//   },
-//   textselectcancel: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 3, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 20,
-//     backgroundColor: '#F1A4A4',
-//     paddingVertical: 5,
-//     top: 20,
-//     right: 90,
-//   },
-//   textselectcoreect: {
-//     paddingHorizontal: 20, // กำหนดระยะห่างภายในกรอบด้านข้าง
-//     paddingVertical: 10, // กำหนดระยะห่างภายในกรอบด้านบนและล่าง
-//     borderWidth: 3, // กำหนดความหนาของกรอบ
-//     borderColor: 'black', // กำหนดสีของกรอบ
-//     borderRadius: 20,
-//     paddingHorizontal: 20,
-//     backgroundColor: '#E8FFC1',
-//     paddingVertical: 5,
-//     top: -22,
-//     left: 80,
-//   }
-// });
